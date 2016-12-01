c5da6702a0117f869f768d7dd47f6673624d6f57
LTR1 <lrothfork@vcd-ch204-16.eastern.ewu.edu>
2016-12-01 14:37:40 -0800

    Weather Version 3
    GeoLocate

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,53 @@ $.simpleWeather({
     }
   
   });
+
+// Get Location
+
+// Get and store Geo Location lat/long coordinates
+if ('geolocation' in navigator) {
+
+   $('.geo button').show(); 
+
+} else {
+  
+  $('.geo button').hide();
+  $('.geo').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('.geo button').click( function() {
+     
+    //load weather using your lat/lng coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+        getWeather(position.coords.latitude+','+position.coords.longitude); 
+    });
+   
+});
+
+// Get geolocated weather
+var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+    // Display Data
+      $('.geo .temp').text(weather.temp + 'F');
+      $('.geo .city').text(weather.city);
+      $('.geo img').attr('src', weather.image);
+        $('.geo .title').text(weather.title);
+        
+      // Entire weather object
+      console.log();
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+    }
+  
+  });
+    
+};
