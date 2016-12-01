fe44ec0da0104b9846448c2772742d2c63824dea
LTR1 <lrothfork@vcd-ch204-16.eastern.ewu.edu>
2016-12-01 13:42:27 -0800

    Weather Version 2
    Added Multiple Cities

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,48 @@
 // My Scripts
+// Spokane
+$.simpleWeather({
+    location: '99004',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.spokane .temp').text(weather.temp);
+      $('.spokane .city').text(weather.city);
+      $('.spokane img').attr('src', weather.image);
+        $('.spokane .title').text(weather.title);
+  
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>Slow or No Connection</p>');
+    }
+  
+  });
+
+
+$.simpleWeather({
+    location: 'seattle',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.seattle .temp').text(weather.temp + 'F');
+      $('.seattle .city').text(weather.city);
+      $('.seattle img').attr('src', weather.image);
+        $('.seattle .title').text(weather.title);
+  
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+        $('body').append('<p>Slow or No Connection</p>');
+    }
+  
+  });
