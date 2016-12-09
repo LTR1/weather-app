4efef5d31de189e32c52f51c1f7026dc0c9b7efb
LTR1 <LyndenRothfork@gmail.com>
2016-12-08 16:46:03 -0800

    Weather Version 2
    Need to get an if statement to work in this to correlate the background with weather.

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,54 @@
 // My Scripts
-// Spokane
-$.simpleWeather({
-    location: '99004',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      
-      // Display Data
-      $('.spokane .temp').text(weather.temp);
-      $('.spokane .city').text(weather.city);
-      $('.spokane img').attr('src', weather.image);
-        $('.spokane .title').text(weather.title);
-  
-      
-      // Entire weather object
-      console.log(weather);
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-        $('body').append('<p>Slow or No Connection</p>');
-    }
-  
-  });
-
-
-$.simpleWeather({
-    location: 'seattle',
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      
-      // Display Data
-      $('.seattle .temp').text(weather.temp + 'F');
-      $('.seattle .city').text(weather.city);
-      $('.seattle img').attr('src', weather.image);
-        $('.seattle .title').text(weather.title);
-  
-      
-      // Entire weather object
-      console.log(weather);
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-        $('body').append('<p>Slow or No Connection</p>');
-    }
-  
-  });
-
-// Get Location
-
-// Get and store Geo Location lat/long coordinates
+var max = 3;
+var randNum = Math.floor(Math.random() * max);
+console.log(randNum);
+//Store Greetings
+var msg0 = 'Hola';
+var msg1 = 'Howdy';
+var msg2 = 'Sup!';
+//Show Greetings
+$('h1 span').text(eval('msg' + randNum));
+//store background classes
+var bg0 = 'bg0';
+var bg1 = 'bg1';
+var bg2 = 'bg2';
+var degree = 0;
+//change background
+$('body').addClass('bg' + randNum);
 if ('geolocation' in navigator) {
-
-   $('.geo button').show(); 
-
-} else {
-  
-  $('.geo button').hide();
-  $('.geo').prepend('<p>Geolocation Not Supported</p>');
-
+    $('.geo button').show();
 }
-
+else {
+    $('.geo button').hide();
+    $('.geo').prepend('<p>Geolocation Not Supported</p>');
+}
 // On Click, Get Geolocation, Call Weather Function
-$('.geo button').click( function() {
-     
+$('.geo button').click(function () {
     //load weather using your lat/lng coordinates
-    navigator.geolocation.getCurrentPosition(function(position) {
-        getWeather(position.coords.latitude+','+position.coords.longitude); 
+    navigator.geolocation.getCurrentPosition(function (position) {
+        getWeather(position.coords.latitude + ',' + position.coords.longitude);
     });
-   
 });
-
 // Get geolocated weather
-var getWeather = function(location) {
-    
-   $.simpleWeather({
-    location: location,
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      
-    // Display Data
-      $('.geo .temp').text(weather.temp + 'F');
-      $('.geo .city').text(weather.city);
-      $('.geo img').attr('src', weather.image);
-        $('.geo .title').text(weather.title);
-        
-      // Entire weather object
-      console.log();
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-    }
-  
-  });
-    
+var getWeather = function (location, degree) {
+    $.simpleWeather({
+        location: location
+        , degree: degree
+        , degree: 24
+        , woeid: ''
+        , unit: 'f'
+        , success: function (weather) {
+            // Display Data
+            console.log(degree);
+            $('.geo .temp').text(weather.temp + 'F');
+            $('.geo .city').text(weather.city);
+            $('.geo img').attr('src', weather.image);
+            $('.geo .title').text(weather.title);
+            // Entire weather object
+            console.log();
+        }
+        , error: function (error) {
+            // Show if weather cannot be retreived
+        }
+    });
 };
